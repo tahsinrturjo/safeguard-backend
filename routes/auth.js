23dd69b46721adea67e3831aa001eb598213b00b
Tahsin Turjo <trturjo06@gmail.com>
2026-05-22 01:20:14 +0600

    Fix model require path to lowercase

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 // POST /auth/register
 router.post('/register', async (req, res) => {
--- routes/sos.js
+++ routes/sos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const admin = require('firebase-admin');
 
 // Set up Firebase (only once)
